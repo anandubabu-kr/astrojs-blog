e39f3d9a0a28a08df5e87cf06c788d90f0cba0a9
anandubabu-kr <41121650+anandubabu-kr@users.noreply.github.com>
2023-07-02 22:24:46 +0530

    architecture changed

---
+++ src/pages/sub/content/javascript/array.md
@@ -0,0 +1,9 @@
+---
+layout: layouts/MarkdownPostLayout.astro
+title: 'Array methodes'
+pubDate: 2023-22-06
+description: ''
+author: 'Anandu Babu'
+tags: ["astro", "blogging", "learning in public"]
+
+---
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      darkBG: '#222831',
+      blueLt: '#2d4059',
+      accentPr: '#ff5722',
+      accentSec: '#f6c90e',
+      ...colors,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
